c26ffba97e60d577295ab67baf92f31431716ce7
mondobizz <caseyjhatley@yahoo.com>
2020-12-20 18:59:01 -0500

    figured out how to type in a song and it only goes through kidz bop artist, just need to connect it to the submit button

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 // IPO Input = -> Process ->  Output
-
+console.log('it is alive!')
 // Constants and State Variables (Data)
-const baseUrl = "https://api.musixmatch.com/ws/1.1/"
+function handleGetData(event){
+    event.preventDefault();
+// https://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz%20bop?&q_track=slide&apikey=ce3e50c2c0465b4676c0a648833e173e
+const baseUrl = "https://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz%20bop?&q_track=slide="
+const searchBop = $("#search").val()
 const apiKey = "ce3e50c2c0465b4676c0a648833e173e"
 
-// Cached Element References
-const response = $.ajax({
-    url: "baseUrl + artist.search?&artist_id=q_artist=kid20%bop& + apiKey"
-})
 
-response.then(
+$.ajax({
+    url: `${baseUrl} + ${searchBop} + ${apiKey}`
+  }).then(
     (data) => {
-        console.log(data);
+     console.log(data);
+     $("#track_name").text(data.TrackName)
     },
     (error) => {
-        console.log("bad request: ", error)
+     console.log('bad request', error);
     }
-)
+  );
+}
+$('form').on("submit", handleGetData)
 // Attached Event Listeners
 
 // Functions
@@ -28,7 +33,7 @@ response.then(
 
 
 // api to search for the track then the artist
-// "http://api.musixmatch.com/ws/1.1/track.search?q_artist=weird al&apikey=ce3e50c2c0465b4676c0a648833e173e"
+// "http://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz20%bop&apikey=ce3e50c2c0465b4676c0a648833e173e"
 
 // api to search the artist then there id
-// https://api.musixmatch.com/ws/1.1/artist.search?&artist_id=q_artist=kid20%bop&apikey=ce3e50c2c0465b4676c0a648833e173e
+// https://api.musixmatch.com/ws/1.1/artist.search?&artist_id=q_artist=kidz20%bop&apikey=ce3e50c2c0465b4676c0a648833e173e
