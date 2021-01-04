573b07b05e04ef58af1f0542c18620afaf57ecd4
mondobizz <caseyjhatley@yahoo.com>
2021-01-03 21:10:27 -0500

    can search through kidz bop songs now

--- js/script.js
+++ js/script.js
@@ -4,19 +4,14 @@ console.log('it is alive!')
 // Constants and State Variables (Data)
 
 // Constant Data
-const baseUrl = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz20%bop?&q_track=slide&apikey=ce3e50c2c0465b4676c0a648833e173e";
+const baseUrl = "https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/";
 const searchUrl = "track.search?q_artist=kidz%20bop?&q_track=";
-const searchBop = $("#search").val();
-const apiKey = "ce3e50c2c0465b4676c0a648833e173e";
+const apiKey = "&apikey=ce3e50c2c0465b4676c0a648833e173e";
 
 // State Data
 let kidzData;
 
-// https://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz%20bop?&q_track=slide&apikey=ce3e50c2c0465b4676c0a648833e173e
-// album_name
-// artist_name
-// Album_release_date
-// Cached Element References
+
 const $searchButton = $('#search-button');
 
 // Attached Event Listeners
@@ -24,54 +19,45 @@ $searchButton.on('click', 'article.album', handleClick);
 
 // Functions
 
+function findSongMatch(data, searchBop) {
+  const trackList = data.message.body.track_list;
+
+  const searchResult = trackList.filter((track) => track.track.track_name.toLowerCase() === searchBop.toLowerCase());
+  console.log(searchResult)
+  if (searchResult.length > 0) {
+    alert(`Kidz Bop covered ${searchBop.toUpperCase()} on ${searchResult[0].track.album_name}!`)
+  } else {
+    alert(`Kidz Bop has not covered ${searchBop}. :(`)
+  }
+}
+
 function getData() {
   // getch data using AJAX
-  $.ajax(baseUrl).then(function(data) {
-    // take the returned data and assign it to a global state variable
-    // call render to visualize it to the DOM 
-    console.log('Data: ', data);
-  }, function(error) {
-        console.log('Error: ', error);
+  const searchBop = $("#search").val();
+  const kidzUrl = baseUrl + searchUrl + searchBop + apiKey
+  console.log(kidzUrl)
+  $.ajax({
+    url: kidzUrl,
+    dataType: "json"
+  }).then(function (data) {
+    findSongMatch(data, searchBop);
+
+  }, function (error) {
+    console.log('Error: ', error);
   });
 }
 
 function handleClick(e) {
   e.preventDefault()
-  alert('click')
+
   getData();
 }
 
+$('form').on("submit", handleClick)
 
 
 
 
 
 
-// function handleGetData(event){
-//     event.preventDefault();
 
-// $.ajax({
-//     url: `${baseUrl} + ${searchUrl} + ${searchBop} + ${apiKey}`
-//   }).then(
-//     (data) => {
-//      console.log(data);
-//      $("#track_name").text(data.TrackName)
-//     },
-//     (error) => {
-//      console.log('bad request', error);
-//     }
-//   );
-// }
-// $('form').on("submit", handleGetData)
-
-// have with images
-// $( ".header-right" ).click(function() {
-//     alert( "Wow, you must really like Kidz Bop" );
-// });
-
-
-// api to search for the track then the artist
-// "http://api.musixmatch.com/ws/1.1/track.search?q_artist=kidz20%bop&apikey=ce3e50c2c0465b4676c0a648833e173e"
-
-// api to search the artist then there id
-// https://api.musixmatch.com/ws/1.1/artist.search?&artist_id=q_artist=kidz20%bop&apikey=ce3e50c2c0465b4676c0a648833e173e
